e95f72afb47689f940cef044031d481e81506879
JF Marronnier <contact@epitez.fr>
2019-08-12 22:51:34 +0200

    Add isRectangle refactor

--- src/triangle.cpp
+++ src/triangle.cpp
@@ -5,6 +5,7 @@
 #include "triangle.h"
 
 #include <stdexcept>
+#include <algorithm>
 
 triangle::triangle(unsigned int length1, unsigned int length2, unsigned int length3) :
         __length{length1, length2, length3} {
@@ -19,7 +20,19 @@ bool triangle::isIsocele() const {
 }
 
 bool triangle::isRectangle() const {
-    return (__length[2] * __length[2]) == (__length[0] * __length[0] + __length[1] * __length[1]) ||
-           (__length[1] * __length[1]) == (__length[0] * __length[0] + __length[2] * __length[2]) ||
-           (__length[0] * __length[0]) == (__length[1] * __length[1] + __length[2] * __length[2]);
+    unsigned int length_max = std::max({__length[0], __length[1], __length[2]});
+    unsigned int length_smaller_1;
+    unsigned int length_smaller_2;
+
+    if (length_max == __length[0]) {
+        length_smaller_1 = __length[1],
+                length_smaller_2 = __length[2];
+    } else if (length_max == __length[1]) {
+        length_smaller_1 = __length[0],
+                length_smaller_2 = __length[2];
+    } else {
+        length_smaller_1 = __length[0],
+                length_smaller_2 = __length[1];
+    }
+    return (length_max * length_max) == (length_smaller_1 * length_smaller_1 + length_smaller_2 * length_smaller_2);
 }
